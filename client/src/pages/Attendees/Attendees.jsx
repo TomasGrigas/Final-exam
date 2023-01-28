e54715b61cf664d1beac75478de2b064fb323100
TomasGrigas <tomas.grigas@icloud.com>
2023-01-28 13:33:17 +0200

    added env variables for React app

--- client/src/pages/Attendees/Attendees.jsx
+++ client/src/pages/Attendees/Attendees.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react"
 import { LOGGED_IN_USER } from "../../constants/constants";
-import { API_URL } from "../../constants/constants";
+
 
 
 export const Attendees = () => {
@@ -9,7 +9,7 @@ export const Attendees = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(()=>{
-        fetch(`${API_URL}/attendees?userId=${LOGGED_IN_USER.id}`)
+        fetch(`${process.env.REACT_APP_API_URL}/attendees?userId=${LOGGED_IN_USER.id}`)
         .then (res => res.json())
         .then(data => {
             setAttendees(data);
