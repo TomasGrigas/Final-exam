1c420829239252d53f4fc076a240d8fa42f0a7bd
TomasGrigas <tomas.grigas@icloud.com>
2023-02-01 21:03:18 +0200

    Added LogOut button functionality

--- client/src/components/PageLayout/PageLayout.jsx
+++ client/src/components/PageLayout/PageLayout.jsx
@@ -1,15 +1,26 @@
 import { useContext } from "react"
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet, useNavigate } from "react-router"
+import { LOCAL_STORAGE_JWT_TOKEN_KEY } from "../../constants/constants"
 import { UserContext } from "../../contexts/UserContextWrapper"
+import { Button } from "../Button/Button"
 
 export const PageLayout= () => {
-    const {user}  = useContext(UserContext);
+    const {user, setUser}  = useContext(UserContext);
+    const navigate = useNavigate();
 
     if (!user) {
         return <Navigate to="/login" />
     } 
+
+    const handleLogOut = () => {
+        localStorage.removeItem(LOCAL_STORAGE_JWT_TOKEN_KEY);
+        setUser(null);
+        navigate('/login');
+    }
+
     return(
         <div>
+            <Button onClick ={handleLogOut}>Log out</Button>
             <Outlet />
         </div>
     )
